bea38300ccc91167f9e18f70da6f2cb39e5b2f62
HzAjax <danil3000200@mail.ru>
2025-06-05 18:58:53 +0400

    Init project

---
+++ src/test/java/ru/volodin/CreditBank/CreditBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volodin.CreditBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
