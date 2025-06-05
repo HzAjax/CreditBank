235f739b4c7562bf6ff1bdf34d504fbd6f4cfc79
HzAjax <danil3000200@mail.ru>
2025-06-05 19:30:17 +0400

    Create DTO

---
+++ calculator/src/main/java/ru/volodin/CreditBank/entity/dto/EmploymentDto.java
@@ -0,0 +1,21 @@
+package ru.volodin.CreditBank.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.volodin.CreditBank.entity.dto.enums.EmploymentStatus;
+import ru.volodin.CreditBank.entity.dto.enums.Position;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmploymentDto {
+    private EmploymentStatus employmentStatus; //enum
+    private String employerINN;
+    private BigDecimal salary;
+    private Position position; //enum
+    private Integer workExperienceTotal;
+    private Integer workExperienceCurrent;
+}
---
+++ calculator/src/main/java/ru/volodin/CreditBank/entity/dto/enums/Position.java
@@ -0,0 +1,6 @@
+package ru.volodin.CreditBank.entity.dto.enums;
+
+public enum Position {
+    MID_MANAGER,
+    TOP_MANAGER
+}
