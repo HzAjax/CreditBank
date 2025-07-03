567c0574ab31bfbd86bd0d35c0056ee2389b34ac
HzAjax <danil3000200@mail.ru>
2025-07-03 23:07:34 +0400

    fix named id deal

--- deal/src/main/java/ru/volodin/deal/controller/DealController.java
+++ deal/src/main/java/ru/volodin/deal/controller/DealController.java
@@ -38,7 +38,7 @@ public class DealController {
     private final DealService dealService;
 
     @PostMapping("/statement")
-    @Operation(summary = "Calculation possiсble offers")
+    @Operation(summary = "Calculation possible offers")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Calculate and save credit"),
             @ApiResponse(responseCode = "400", description = "Invalid format",
